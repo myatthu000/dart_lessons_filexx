3c46f2604c0e868d2040b533670d672337c37e06
myatthu000 <september.2019thu@gmail.com>
2025-07-17 19:26:59 +0630

    var-typ

---
+++ lesson1.dart
@@ -0,0 +1,21 @@
+void main() {
+  //Variables
+//   print(19);
+
+//   print(10*2);
+//   print(10-2);
+//   print(10-2);
+//   print(10+2);
+//   print(10%3);  // remainder
+//   print(10/2);  // division
+
+  // <datatype> <variable> = value;
+  int firstValue = 23;
+  print('first value: $firstValue ');
+
+  int someRandomNo = 89;
+  print(someRandomNo.bitLength);
+
+  // Variables
+  // int String Object late final const var double
+}
